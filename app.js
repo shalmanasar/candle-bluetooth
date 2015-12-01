e006c121de7129a65510c5f10e4e63c9c4dee240
Sid Carpenter <gunnar.heim@gmail.com>
2015-12-01 02:15:52 -0700

    Update app.js

--- app.js
+++ app.js
@@ -13,6 +13,16 @@ document.querySelector('#connect').addEventListener('click', event => {
   });
 });
 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js').then(function(registration) {
+    // Registration was successful
+    console.log('ServiceWorker registration successful with scope: ',    registration.scope);
+  }).catch(function(err) {
+    // registration failed :(
+    console.log('ServiceWorker registration failed: ', err);
+  });
+}
+
 function handleDeviceName(deviceName) {
   document.querySelector('#deviceName').value = deviceName;
 }
